ee2d9e1bd6b386a11d9279e43c28265d6caeccf3
Qiu Xiang <xiang.qiu@qq.com>
2015-04-06 23:27:34 +0800

    删掉无用代码

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ class App:
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.indicator.set_menu(self.menu)
 
-        menu_item = Gtk.SeparatorMenuItem()
-        menu_item.show()
-        self.menu.append(menu_item)
-
         menu_item = Gtk.MenuItem('退出')
         menu_item.show()
         menu_item.connect('activate', Gtk.main_quit)
@@ -38,34 +34,14 @@ class App:
         Gtk.main()
 
     def update_traffics(self):
-        for device in self.hifiwi.devices()['devices']:
-            self.devices[device['ip']] = device
-
         while True:
             try:
                 data = self.hifiwi.traffics()['data']
                 self.indicator.set_label('↑ %s KB/S    ↓ %s KB/S' % (
                     data['total']['up'], data['total']['down']), '')
-
-                # print('\033c', end='')
-                # for item in data['list']:
-                #     if item['ip'] in self.devices:
-                #         print('↑ %s KB/S\t↓ %s KB/S\t%s' % (
-                #             item['up'], item['down'], self.devices[item['ip']]['name']), '')
-
                 sleep(4)
             except ConnectionError as error:
                 print(error)
 
-    @staticmethod
-    def create_menu_item(label):
-        menu_item = Gtk.MenuItem(label)
-        menu_item.show()
-        return menu_item
-
 if __name__ == '__main__':
-    # import sys
-    # reload(sys)
-    # sys.setdefaultencoding('utf-8')
-
     app = App()
