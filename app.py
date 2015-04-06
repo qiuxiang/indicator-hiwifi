675dada71541a403dd721e3928691348b0a7c935
Qiu Xiang <xiang.qiu@qq.com>
2015-04-06 23:26:00 +0800

    下拉菜单的实现有 bug，而且效率不好，暂时只显示总流量

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 from os import path
-from requests import ConnectionError
-from threading import Thread
 from time import sleep
+from threading import Thread
+from requests import ConnectionError
 from gi.repository import Gtk, AppIndicator3
 from hiwifi import Hiwifi
 
@@ -12,7 +12,7 @@ class App:
         self.menu = Gtk.Menu()
 
         self.indicator = AppIndicator3.Indicator.new(
-            'example-simple-client',
+            'indicator-hiwifi',
             path.abspath(path.dirname(__file__)) + '/icon.svg',
             AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
@@ -39,23 +39,6 @@ class App:
 
     def update_traffics(self):
         for device in self.hifiwi.devices()['devices']:
-            submenu_item_up = self.create_menu_item('上传：')
-            submenu_item_down = self.create_menu_item('下载：')
-
-            submenu = Gtk.Menu()
-            submenu.append(self.create_menu_item('IP：' + device['ip']))
-            submenu.append(self.create_menu_item('MAC：' + device['mac']))
-            submenu.append(self.create_menu_item('连接类型：' + device['type']))
-            submenu.append(submenu_item_up)
-            submenu.append(submenu_item_down)
-
-            menu_item = self.create_menu_item(device['name'])
-            menu_item.set_submenu(submenu)
-            self.menu.prepend(menu_item)
-
-            device['menu_item'] = menu_item
-            device['submenu_item_up'] = submenu_item_up
-            device['submenu_item_down'] = submenu_item_down
             self.devices[device['ip']] = device
 
         while True:
@@ -64,11 +47,11 @@ class App:
                 self.indicator.set_label('↑ %s KB/S    ↓ %s KB/S' % (
                     data['total']['up'], data['total']['down']), '')
 
-                for item in data['list']:
-                    if item['ip'] in self.devices:
-                        device = self.devices[item['ip']]
-                        device['submenu_item_up'].set_label('上传：%s KB/S' % item['up'])
-                        device['submenu_item_down'].set_label('下载：%s KB/S' % item['down'])
+                # print('\033c', end='')
+                # for item in data['list']:
+                #     if item['ip'] in self.devices:
+                #         print('↑ %s KB/S\t↓ %s KB/S\t%s' % (
+                #             item['up'], item['down'], self.devices[item['ip']]['name']), '')
 
                 sleep(4)
             except ConnectionError as error:
